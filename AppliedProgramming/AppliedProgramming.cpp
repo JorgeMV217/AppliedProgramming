ecd5952fe3ac9454905e0954042bb4e6d5254b3e
jmadr <jmadr@GAMING_PC>
2024-11-26 15:16:11 +0100

    Agregar archivos de proyecto.

---
+++ AppliedProgramming/AppliedProgramming.cpp
@@ -0,0 +1,12 @@
+﻿// AppliedProgramming.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "AppliedProgramming.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
